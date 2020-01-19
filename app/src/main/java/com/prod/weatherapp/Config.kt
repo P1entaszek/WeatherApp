457bd38433570540af43d1570c6c9c0ce527388d
Piotr Jaszczurowski <piotrjaszczurowski87@gmail.com>
2020-01-19 17:02:03 +0100

    Initilized repository, get user localization, add permissions +  permission request in activity

---
+++ app/src/main/java/com/prod/weatherapp/Config.kt
@@ -0,0 +1,9 @@
+package com.prod.weatherapp
+
+/**
+ * Created by Piotr Jaszczurowski on 19.01.2020
+ */
+object Config {
+    val openWeatherMapApiEndpoint = "api.openweathermap.org/data/2.5/forecast"
+    val openWeatherMapApiKey = "6e2a2642803b430de88ccd5137a83066"
+}
